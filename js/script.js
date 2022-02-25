09eb24f4c338a00ab5afe92ab4aeccd266df9cb9
Prince Roy <princeku.ray143@gmail.com>
2022-02-26 00:04:06 +0530

    validation fixed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const form = document.querySelector(".form");
+const form = document.querySelector("#form");
 const myAlert = document.querySelector(".alert");
 const email = document.querySelector(".email");
 
@@ -6,22 +6,23 @@ function isValidEmail(userEmail) {
     const regexEmail =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    regexEmail.test(userEmail);
+    return regexEmail.test(userEmail);
 }
 
 function validateField() {
     const emailValue = email.value;
+    console.log(email);
 
     if (isValidEmail(emailValue)) {
-        myAlert.innerHTML = "";
+        myAlert.innerText = "";
+        myAlert.classList.remove("invalid");
     } else {
-        myAlert.innerHTML = "Please provide a valid email";
+        myAlert.innerText = "Please provide a valid email";
         email.classList.add("invalid");
     }
 }
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
     validateField();
+    event.preventDefault();
 });
